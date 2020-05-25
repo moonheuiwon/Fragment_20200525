3d9ce7ae50c3671ccba0f6dd5a014f29741e43dc
moonheuiwon <wonheuimoon@gmail.com>
2020-05-25 16:38:44 +0900

    프래그먼트에 데이터 바인딩 + onActivityCreated에서 활용

--- app/src/main/java/com/example/fragment_20200525/fragments/FirstFragment.java
+++ app/src/main/java/com/example/fragment_20200525/fragments/FirstFragment.java
@@ -7,19 +7,33 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.fragment_20200525.R;
+import com.example.fragment_20200525.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
+
+    FragmentFirstBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = binding.messageEdt.getText().toString();
+                binding.myTxt.setText(input);
+            }
+        });
     }
 }
